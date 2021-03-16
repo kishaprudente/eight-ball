57ba4f293ad28f18fe67929d1d6db0dd504904bc
Kisha Prudente <kisha.prudente@gmail.com>
2021-03-15 21:59:44 -0400

    refactor(theme): add custom theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import { Container } from '@material-ui/core';
+import {
+	unstable_createMuiStrictModeTheme as createMuiTheme,
+	ThemeProvider,
+} from '@material-ui/core/styles';
 import Title from './components/Title';
 import Image from './components/Image';
 import Body from './components/Body';
 
+const theme = createMuiTheme({
+	scheme: {
+		primary: '#f25f25',
+		secondary: '#25b8f2',
+		color: '#fff',
+	},
+	font: 'Josefin Sans',
+});
+
 const App = () => {
 	return (
-		<Container className='App'>
-			<Title />
-			<Image />
-			<Body />
-		</Container>
+		<>
+			<ThemeProvider theme={theme}>
+				<Container className='App'>
+					<Title />
+					<Image />
+					<Body />
+				</Container>
+			</ThemeProvider>
+		</>
 	);
 };
 
